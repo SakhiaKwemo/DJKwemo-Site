1af043fd23032ce748c63670db8ca01c7b0d952c
SakhiaKwemo <45702852+SakhiaKwemo@users.noreply.github.com>
2021-06-28 14:48:33 -0400

    Add files via upload

---
+++ src/Pages/Music/Selector/Custom/Item.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Item.css';
+
+export default class Item extends Component {
+
+  render(){
+    return (
+      <div id="itemBox"> 
+      </div>
+    );
+  }
+}
+
+
+
---
+++ src/Pages/Music/Selector/Custom/Schedule.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './Schedule.css';
+
+export default class Schedule extends Component {
+  
+  render(){
+    return (
+      <div id="ScheduleBox"> 
+        <div id = "inputBox3">
+          <text>Custom</text>
+        </div>
+        <div className = "scheduleContent2">
+          <text>Coming Soon!!</text>
+        </div>
+      </div>
+    );
+  }
+}
+
