c13642d696ab34574100475bd89d1076e554ff04
SakhiaKwemo <45702852+SakhiaKwemo@users.noreply.github.com>
2021-06-28 14:58:01 -0400

    Add files via upload

---
+++ src/Pages/Books/AudioBook/AudioBook.js
@@ -0,0 +1,27 @@
+import React, {Component, useState, useEffect} from 'react';
+import './AudioBook.css';
+
+import {FaSearch} from 'react-icons/fa';
+
+import axios from "axios"; 
+
+export default function AudioBook() {
+  
+  {
+    return (
+      <div id="PlaylistsBox"> 
+        <div id = "inputBox4">
+          <text>Audio Books</text>
+        </div>
+        <div className = "scheduleContent">
+          <div className = "bookContainer23">
+            <img src = "\images\Book\I Will Become\I Will Become Book.png" className = "bookBox2"/>
+            <text>DJKwemo</text>
+            <p>"I Will Become"</p>
+          </div>
+          <text className = "warningText">It will soon be available!!!</text>
+        </div>
+      </div>
+    );
+  }
+}
