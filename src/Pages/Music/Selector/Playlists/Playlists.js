ef6ba4df37851ef8a2609eacc2d3fa4f0e7e6a84
SakhiaKwemo <45702852+SakhiaKwemo@users.noreply.github.com>
2021-06-28 14:50:13 -0400

    Add files via upload

---
+++ src/Pages/Music/Selector/Playlists/Playlists.js
@@ -0,0 +1,27 @@
+import React, {Component, useState, useEffect} from 'react';
+import './Playlists.css';
+
+import {FaSearch} from 'react-icons/fa';
+
+import axios from "axios"; 
+
+export default function Playlists() {
+  
+  {
+    return (
+      <div id="PlaylistsBox"> 
+        <div id = "inputBox4">
+          <text>Playlists</text>
+        </div>
+        <div className = "scheduleContent1">
+          <text>Coming Soon!!</text>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+
